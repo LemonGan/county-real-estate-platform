03c6193a25b19dbbecb69e8dd93a77dd2099dd04
LemonGan <635238474@qq.com>
2026-05-09 21:43:58 +0800

    fix: baseUrl改回8881端口

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -3,7 +3,7 @@ App({
   globalData: {
     userInfo: null,
     token: null,
-    baseUrl: 'https://8.138.129.142/api/v1', // 生产环境API地址(HTTPS)
+    baseUrl: 'http://8.138.129.142:8881/api/v1', // 生产环境API地址
     isLogin: false, 
     location: null
   },
